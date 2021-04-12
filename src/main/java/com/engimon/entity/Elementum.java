da888772a6a2e5ebfdc7f998857f85812160609f
ravielze <ravielz20@gmail.com>
2021-04-13 03:05:42 +0700

    Elementum blueprint

--- src/main/java/com/engimon/entity/Elementum.java
+++ src/main/java/com/engimon/entity/Elementum.java
@@ -5,33 +5,47 @@ public abstract class Elementum {
     private Element firstElement;
     private Element secondElement;
 
-    public Elementum(Element firstElement){
-        if (firstElement == Element.NONE){
+    public Elementum(Element firstElement) throws IllegalArgumentException {
+        if (firstElement == Element.NONE) {
             throw new IllegalArgumentException("First element cannot be none.");
         }
         this.firstElement = firstElement;
         this.secondElement = Element.NONE;
 
     }
-    public Elementum(Element firstElement, Element secondElement){
-        if (firstElement == Element.NONE){
+
+    public Elementum(Element firstElement, Element secondElement) throws IllegalArgumentException {
+        if (firstElement == Element.NONE) {
             throw new IllegalArgumentException("First element cannot be none.");
         }
         this.firstElement = firstElement;
         this.secondElement = secondElement;
     }
 
-
-    public int getElements(){
+    public int getElements() {
         return 1 + (this.secondElement == Element.NONE ? 0 : 1);
     }
 
-    public Element getFirstElement(){
+    public Element getFirstElement() {
         return this.firstElement;
     }
 
-    public Element getSecondElement(){
+    public Element getSecondElement() {
         return this.secondElement;
     }
-    
+
+    public boolean isOneOf(Element el) {
+        return el == this.firstElement || el == this.secondElement;
+    }
+
+    public Element getMajorElement(Elementum elementum) {
+        // TODO tunggu ada mapping element
+        return Element.NONE;
+    }
+
+    public Element getMinorElement(Elementum elementum) {
+        // TODO tunggu ada mapping element
+        return Element.NONE;
+    }
+
 }
