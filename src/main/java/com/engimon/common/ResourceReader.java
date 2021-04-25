ac9653540d77e545c7e96de7b963d81f9633d0f7
ravielze <ravielz20@gmail.com>
2021-04-25 13:35:48 +0700

    Add resource reader file

--- src/main/java/com/engimon/common/ResourceReader.java
+++ src/main/java/com/engimon/common/ResourceReader.java
@@ -1,14 +1,18 @@
 package com.engimon.common;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 public final class ResourceReader {
 
     private InputStream inputStream;
+    private File file;
     private String fileName;
 
     public ResourceReader(String fileName) {
@@ -16,13 +20,17 @@ public final class ResourceReader {
 
         // The class loader that loaded the class
         ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(fileName);
         InputStream inputStream = classLoader.getResourceAsStream(fileName);
 
         // the stream holding the file content
-        if (inputStream == null) {
+        if (inputStream == null || resource == null) {
             throw new IllegalArgumentException("file not found! " + fileName);
         }
         this.inputStream = inputStream;
+        try{
+            this.file = new File(resource.toURI());
+        } catch (URISyntaxException ignored){}
     }
 
     public final String getFilename() {
@@ -35,6 +43,10 @@ public final class ResourceReader {
 
     public final InputStreamReader getStreamReader() {
         return new InputStreamReader(this.inputStream, StandardCharsets.UTF_8);
+    }
+
+    public final File getFile(){
+        return this.file;
     }
 
     public final void printInputStream() {
