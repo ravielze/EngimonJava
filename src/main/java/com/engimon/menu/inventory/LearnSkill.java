18f59b14bcd83bbeebaededef7c8025462ee7a9e
ravielze <ravielz20@gmail.com>
2021-04-26 20:36:32 +0700

    Add skillitem expired

--- src/main/java/com/engimon/menu/inventory/LearnSkill.java
+++ src/main/java/com/engimon/menu/inventory/LearnSkill.java
@@ -5,11 +5,13 @@ import javax.swing.JOptionPane;
 import com.engimon.entity.engimon.Engimon;
 import com.engimon.entity.skill.SkillItem;
 import com.engimon.exception.EngimonStateException;
+import com.engimon.exception.SkillItemExpired;
 import com.engimon.menu.choose_engimon.ChooseEngimon;
 import com.engimon.menu.choose_engimon.EngimonCard;
 
 public class LearnSkill extends ChooseEngimon {
     private SkillItem skillItem;
+
     public LearnSkill(SkillItem skillItem) {
         super(ChooseMode.ENGIMON);
         this.skillItem = skillItem;
@@ -21,14 +23,17 @@ public class LearnSkill extends ChooseEngimon {
         try {
             skillItem.learn(e);
         } catch (EngimonStateException er) {
-            JOptionPane.showMessageDialog(this, String.format("%s cant't learn that skill", e.getName()), "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(this, String.format("%s cant't learn that skill", e.getName()), "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        } catch (SkillItemExpired se) {
+            // TODO
         }
     }
 
     @Override
     public void chooseSkillItem(SkillItem e, EngimonCard ec) {
         // TODO Auto-generated method stub
-        
+
     }
-    
+
 }
